6bace133c60c44646bf49b667e38311bb9f551fd
Gustavo Domingos Cortelassi <gutocortelassi72@gmail.com>
2024-04-03 19:42:33 -0300

    Arquivos base projeto 1.0
    Arquivos base

---
+++ YonderBase/YonderBase/src/test/java/com/Grupo3/YonderBase/YonderBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Grupo3.YonderBase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YonderBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
