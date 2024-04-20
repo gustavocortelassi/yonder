5e2f9a7ae3b7e23e7c49490fca65c4b7d88339fc
João Vitor Candido <joaovitorcand@outlook.com>
2024-04-19 21:52:47 -0300

    delet de desnecessidade

--- YonderBase/YonderBase/src/main/java/com/Grupo3/YonderBase/controller/NivelamentoUsuarioController.java
+++ YonderBase/YonderBase/src/main/java/com/Grupo3/YonderBase/controller/NivelamentoUsuarioController.java
@@ -1,5 +1,30 @@
 package com.Grupo3.YonderBase.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.Grupo3.YonderBase.service.NivelamentoUsuarioService;
+
+@RestController
 public class NivelamentoUsuarioController {
+
+
+
+        @Autowired
+    private NivelamentoUsuarioService nivusser;
+
+
+    Long id = (long) 1;
+
+
+    @GetMapping("/notas")
+    public Long mostranota(@PathVariable Long id) {
+        
+        return nivusser.obternota(id);
+    }
+
+
     
 }
--- YonderBase/YonderBase/src/main/java/com/Grupo3/YonderBase/service/NivelamentoUsuarioService.java
+++ YonderBase/YonderBase/src/main/java/com/Grupo3/YonderBase/service/NivelamentoUsuarioService.java
@@ -1,5 +1,21 @@
 package com.Grupo3.YonderBase.service;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.Grupo3.YonderBase.model.NivelamentoUsuario;
+import com.Grupo3.YonderBase.repository.NivelamentoUsuarioRepository;
+
 public class NivelamentoUsuarioService {
+
+@Autowired
+private NivelamentoUsuarioRepository nivelamentoUsuarioRepository;
+
+        public Long obternota(Long id) {
+        NivelamentoUsuario nivelamentoUsuario = nivelamentoUsuarioRepository.findByLongId(id);
+            return nivelamentoUsuario.getNiveisId();
+
+    }
     
 }
