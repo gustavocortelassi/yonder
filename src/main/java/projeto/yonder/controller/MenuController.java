210655e61f3ab51e27a18b09c2a8bfc2007f6db4
Igor <igorggwpp@gmail.com>
2024-05-27 18:57:08 -0300

    resolvendo conflitos da main volta de commit

--- src/main/java/projeto/yonder/controller/MenuController.java
+++ src/main/java/projeto/yonder/controller/MenuController.java
@@ -4,14 +4,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-
-
 @Controller
 public class MenuController {
     
     @GetMapping("/")
     public String home(Model model){
         return "TelaMenu";
+    }
+
+    @GetMapping("/preencher")
+    public String prencher() {
+
+        return "TelaReadingPreenchimento";
     }
 }
 
