25aad8442dd0fec3ab5376662577b097209821ea
MestreDoPortal <joaovitorcand@outlook.com>
2024-06-09 19:08:32 -0300

    pequenos ajustes
    n vi q tava na main, mas nenhuma alteração grande q danifique algo

--- src/main/java/projeto/yonder/controller/PreenchimentoController.java
+++ src/main/java/projeto/yonder/controller/PreenchimentoController.java
@@ -1,17 +1,15 @@
 package projeto.yonder.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class MenuController {
-    
-    @GetMapping("/")
-    public String home(Model model){
-        return "TelaMenu";
-    }
+public class PreenchimentoController {
 
+    @GetMapping("/preencher")
+    public String prencher() {
+
+        return "TelaReadingPreenchimento";
+    }
     
 }
-
