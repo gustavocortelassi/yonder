97a45fc49097d5135a3bedfd2caabc61069e88af
Vitor Parente <vitorebparente@hotmail.com>
2024-06-14 16:15:24 -0300

    Controller Listening feito

--- src/main/java/projeto/yonder/controller/ListeningController.java
+++ src/main/java/projeto/yonder/controller/ListeningController.java
@@ -3,9 +3,9 @@ package projeto.yonder.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
 import projeto.yonder.model.Pergunta;
+import projeto.yonder.model.Respostas;
 import projeto.yonder.model.Usuario;
 import projeto.yonder.repository.PerguntaRepository;
 import projeto.yonder.repository.RespostasRepository;
@@ -15,6 +15,7 @@ import projeto.yonder.service.UsuarioService;
 import static org.springframework.data.jpa.domain.AbstractPersistable_.id;
 
 @Controller
+@RequestMapping("/listening/{userId}")
 public class ListeningController {
 
     @Autowired
@@ -25,18 +26,29 @@ public class ListeningController {
     @Autowired
     private RespostasRepository respostasRepository;
 
-    @GetMapping("/listening/{id}")
-    public String ExibirFormulario(Model model, @PathVariable Long id) {
-        Usuario usuario = usuarioService.getUsuarioById(id);
-        Pergunta pergunta = perguntaRepository.findById(id).orElse(null);
+    @GetMapping("/question/{id}")
+    public String ExibirFormulario(Model model, @PathVariable Long userId, @PathVariable Long perguntaId) {
+        Usuario usuario = usuarioService.getUsuarioById(userId);
+        Pergunta pergunta = perguntaRepository.findById(perguntaId).orElse(null);
 
         if (pergunta != null) {
             model.addAttribute("pergunta", pergunta);
             model.addAttribute("respostas", pergunta.getRespostas());
+            model.addAttribute("userId", userId);
+            model.addAttribute("proximaPerguntaId", perguntaId);
+        } else {
+            model.addAttribute("mensagem", "Não há mais perguntas disponíveis.");
+            return "TelaResultado";
         }
-
         return "TelaProvaListening";
+    }
 
+    @PostMapping("/question/{id}")
+    public String processarFormulario(@PathVariable Long userId, @PathVariable Long perguntaId, @RequestParam("resposta") Long respostaId) {
+        Usuario usuario = usuarioService.getUsuarioById(userId);
+        Pergunta pergunta = perguntaRepository.findById(perguntaId).orElse(null);
+        Respostas resposta = respostasRepository.findById(respostaId).orElse(null);
+        return "redirect:/listening/" + userId + "/question/" + (perguntaId + 1);
     }
 
 }
